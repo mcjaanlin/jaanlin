b4e21ab9ad4cd5a5fae255debf2c81835e74f27f
H'ng Jaan Lin <hngjaanlin@gmail.com>
2021-02-09 15:55:38 +0800

    Update test.js

--- test.js
+++ test.js
@@ -122,7 +122,7 @@ function ajaxCall(){
 	
 	
 	$.ajax({
-	  url: 'http://101.99.84.74/bookhotel',
+	  url: 'http://101.99.84.74/bookhotel/testajax',
 	  crossDomain: true,
 	  error: function(jqXHR, textStatus, errorThrown) {
 		alert(textStatus + ": "+jqXHR.status +" " + errorThrown);
